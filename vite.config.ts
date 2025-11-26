c0e8c64ba88e027dc3fe19259be5bf75cdd01664
Markwang0203 <ab830203@gmail.com>
2025-11-27 04:05:08 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    root: '.' // 
+    root: '.', // 
     plugins: [react()],
     // ...
   }
